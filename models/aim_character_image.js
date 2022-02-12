c4f5d429bc9386131f17fbc8380b64c911fd3629
fredkeemhaus <fredkeemhaus@gmail.com>
2022-02-13 01:01:34 +0900

    change api

--- models/aim_character_image.js
+++ models/aim_character_image.js
@@ -1,25 +1,31 @@
-const Sequelize =require('sequelize');
-const Aim_character_image = require('./aim_character_image');
+const Sequelize = require("sequelize");
+const Aim_character_image = require("./aim_character_image");
 
 module.exports = class Aim_user_info extends Sequelize.Model {
-    static init(sequelize) {
-        return super.init({
-            images: {
-                type:Sequelize.STRING(512),
-                defaultValue: NULL
-            },
-        }, {
-            sequelize,
-            timestamps: true,
-            underscored: false,
-            modelName: 'Aim_user_info',
-            tableName: 'aim_user_info',
-            paranoid: false,
-            charset: 'utf8',
-            collate: 'utf8_general_ci',
-        });
-    }
-    static associate(db) {
-        db.Aim_user_info.hasMany(db.Aim_map_images, { foreignKey: 'host_id', sourceKey:'id'});
-    }
+  static init(sequelize) {
+    return super.init(
+      {
+        images: {
+          type: Sequelize.STRING(512),
+          defaultValue: NULL,
+        },
+      },
+      {
+        sequelize,
+        timestamps: true,
+        underscored: false,
+        modelName: "Aim_user_info",
+        tableName: "aim_user_info",
+        paranoid: false,
+        charset: "utf8",
+        collate: "utf8_general_ci",
+      }
+    );
+  }
+  static associate(db) {
+    db.Aim_user_info.hasMany(db.Aim_map_images, {
+      foreignKey: "hostId",
+      sourceKey: "id",
+    });
+  }
 };
--- models/aim_map_images.js
+++ models/aim_map_images.js
@@ -1,44 +1,50 @@
-const Sequelize =require('sequelize');
-const Aim_user_info = require('../models/aim_user_info');
+const Sequelize = require("sequelize");
+const Aim_user_info = require("../models/aim_user_info");
 module.exports = class Aim_map_images extends Sequelize.Model {
-    static init(sequelize) {
-        return super.init({
-            /* 시퀄라이즈는 알아서 id를 기본키로 연결하므로 id컬럼은 넣을 필요없다. */
-            // id: {
-            //     type:Sequelize.INTEGER.UNSIGNED(11),
-            //     primaryKey: true, 
-            //     allowNull:false,
-            //     unique: true,
-            //     autoIncrement: true,
-            // },
-            host_id: {
-                type:Sequelize.STRING(45),
-                allowNull: false,
-            },
-            map_s3_addr: {
-                type:Sequelize.STRING(200),
-                defaultValue: "NULL",
-            },
-            title: {
-                type:Sequelize.STRING(100),
-                defaultValue: "NULL",
-            },
-            desc: {
-                type:Sequelize.STRING(100),
-                defaultValue: "NULL",
-            },
-        }, {
-            sequelize,
-            timestamps: true,
-            underscored: false,
-            modelName: 'Aim_map_images',
-            tableName: 'aim_map_images',
-            paranoid: false,
-            charset: 'utf8',
-            collate: 'utf8_general_ci',
-        });
-    }
-    static associate(db) {
-        db.Aim_map_images.belongsTo(db.Aim_user_info, { foreignKey: 'host_id', targetKey:'id'});
-    }
+  static init(sequelize) {
+    return super.init(
+      {
+        /* 시퀄라이즈는 알아서 id를 기본키로 연결하므로 id컬럼은 넣을 필요없다. */
+        // id: {
+        //     type:Sequelize.INTEGER.UNSIGNED(11),
+        //     primaryKey: true,
+        //     allowNull:false,
+        //     unique: true,
+        //     autoIncrement: true,
+        // },
+        hostId: {
+          type: Sequelize.INTEGER(11),
+          allowNull: false,
+        },
+        image: {
+          type: Sequelize.STRING(200),
+          defaultValue: "NULL",
+        },
+        title: {
+          type: Sequelize.STRING(100),
+          defaultValue: "NULL",
+        },
+        desc: {
+          type: Sequelize.STRING(100),
+          defaultValue: "NULL",
+        },
+      },
+      {
+        sequelize,
+        timestamps: true,
+        underscored: false,
+        modelName: "Aim_map_images",
+        tableName: "aim_map_images",
+        paranoid: false,
+        charset: "utf8",
+        collate: "utf8_general_ci",
+      }
+    );
+  }
+  static associate(db) {
+    db.Aim_map_images.belongsTo(db.Aim_user_info, {
+      foreignKey: "hostId",
+      targetKey: "id",
+    });
+  }
 };
