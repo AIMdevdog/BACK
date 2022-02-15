0d8d8ab01fa3352a7cb543d874b40115b20ff421
fredkeemhaus <fredkeemhaus@gmail.com>
2022-02-16 02:49:47 +0900

    origin

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ const userRoomRouter = require("./routes/userRoom");
 const app = express();
 
 const httpServer = http.createServer(app);
-const PORT = 8000;
+const PORT = 5000;
+const HOST = "0.0.0.0";
 const io = require("socket.io")(httpServer, {
   cors: {
     origin: ["https://dev-team-aim.com:*"],
@@ -123,7 +124,7 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-httpServer.listen(PORT, () => {
+httpServer.listen(PORT, HOST, () => {
   console.log(`Server running on ${PORT}`);
 });
 
