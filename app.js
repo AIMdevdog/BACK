8549c46ac372dc1de13829049eb179e1fca6960b
gitddabong <wonjong0701@naver.com>
2022-02-24 22:49:45 +0900

    sfu first commit

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const { sequelize } = require("./models");
+const mediasoup = require("mediasoup");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
@@ -21,12 +22,13 @@ const app = express();
 
 const httpServer = http.createServer(app);
 const PORT = 8000;
-const io = require("socket.io")(httpServer, {
+const server = require("socket.io")(httpServer, {
   // cors: {
   //   origin: "*",
   //   credentials: true,
   // },
 });
+const io = server.of('/mediasoup')
 
 // express앱과 MySQL을 연결
 sequelize
@@ -75,6 +77,14 @@ let groupObjArr = [
   //   ],
   // },
 ];
+
+// WebRTC SFU (mediasoup)
+let worker;
+let rooms = {};          // { roomName1: { Router, rooms: [ sicketId1, ... ] }, ...}
+let peers = {};          // { socketId1: { roomName1, socket, transports = [id1, id2,] }, producers = [id1, id2,] }, consumers = [id1, id2,], peerDetails }, ...}
+let transports = [];     // [ { socketId1, roomName1, transport, consumer }, ... ]
+let producers = [];      // [ { socketId1, roomName1, producer, }, ... ]
+let consumers = [];      // [ { socketId1, roomName1, consumer, }, ... ]
 
 app.use(cors(corsOption));
 app.use(express.static("public"));
@@ -245,7 +255,46 @@ function broadcastState() {
 updateGame();
 broadcastState();
 
+// WebRTC SFU (mediasoup)
+const createWorker = async () => {
+  worker = await mediasoup.createWorker({
+    rtcMinPort: 2000,
+    rtcMaxPort: 2020,
+  })
+  console.log(`worker pid ${worker.pid}`)
 
+  worker.on('died', error => {
+    // This implies something serious happened, so kill the application
+    console.error('mediasoup worker has died')
+    setTimeout(() => process.exit(1), 2000) // exit in 2 seconds
+  })
+
+  return worker
+}
+
+// We create a Worker as soon as our application starts
+worker = createWorker()
+
+// This is an Array of RtpCapabilities
+// https://mediasoup.org/documentation/v3/mediasoup/rtp-parameters-and-capabilities/#RtpCodecCapability
+// list of media codecs supported by mediasoup ...
+// https://github.com/versatica/mediasoup/blob/v3/src/supportedRtpCapabilities.ts
+const mediaCodecs = [
+  {
+    kind: 'audio',
+    mimeType: 'audio/opus',
+    clockRate: 48000,
+    channels: 2,
+  },
+  {
+    kind: 'video',
+    mimeType: 'video/VP8',
+    clockRate: 90000,
+    parameters: {
+      'x-google-start-bitrate': 1000,
+    },
+  },
+]
 
 io.on("connection", function (socket) {
   console.log(`${socket.id} has joined!`);
@@ -409,6 +458,178 @@ io.on("connection", function (socket) {
  // 그룹 넘버 초기화
     removeUser(removeSid);
   });
+
+  // WebRTC SFU (mediasoup)
+  socket.on('getRtpCapabilities', async (callback) => {
+    const router1 = await createRoom(roomName, socket.id);
+
+    peers[socket.id] = {
+      socket,
+      roomName,           // Name for the Router this Peer joined
+      transports: [],
+      producers: [],
+      consumers: [],
+      peerDetails: {
+        name: '',
+        isAdmin: false,   // Is this Peer the Admin?
+      }
+    }
+
+    // get Router RTP Capabilities
+    const rtpCapabilities = router1.rtpCapabilities
+
+    // call callback from the client and send back the rtpCapabilities
+    callback({ rtpCapabilities })
+  })
+
+  const createRoom = async (roomName, socketId) => {
+    // worker.createRouter(options)
+    // options = { mediaCodecs, appData }
+    // mediaCodecs -> defined above
+    // appData -> custom application data - we are not supplying any
+    // none of the two are required
+    let router1
+    let peers = []
+    if (rooms[roomName]) {
+      router1 = rooms[roomName].router
+      peers = rooms[roomName].peers || []
+    } else {
+      router1 = await worker.createRouter({ mediaCodecs, })
+    }
+    
+    console.log(`Router ID: ${router1.id}`, peers.length)
+
+    rooms[roomName] = {
+      router: router1,
+      peers: [...peers, socketId],
+    }
+
+    return router1
+  }
+
+  socket.on('createWebRtcTransport', async ({ consumer }, callback) => {
+    // get Room Name from Peer's properties
+    const roomName = peers[socket.id].roomName
+
+    // get Router (Room) object this peer is in based on RoomName
+    const router = rooms[roomName].router
+
+
+    createWebRtcTransport(router).then(
+      transport => {
+        callback({
+          params: {
+            id: transport.id,
+            iceParameters: transport.iceParameters,
+            iceCandidates: transport.iceCandidates,
+            dtlsParameters: transport.dtlsParameters,
+          }
+        })
+
+        // add transport to Peer's properties
+        addTransport(transport, roomName, consumer)
+      },
+      error => {
+        console.log(error)
+      })
+  })
+
+  const addTransport = (transport, roomName, consumer) => {
+
+    transports = [
+      ...transports,
+      { socketId: socket.id, transport, roomName, consumer, }
+    ]
+
+    peers[socket.id] = {
+      ...peers[socket.id],
+      transports: [
+        ...peers[socket.id].transports,
+        transport.id,
+      ]
+    }
+  }
+
+  // see client's socket.emit('transport-connect', ...)
+  socket.on('transport-connect', ({ dtlsParameters }) => {
+    console.log('DTLS PARAMS... ', { dtlsParameters })
+    
+    getTransport(socket.id).connect({ dtlsParameters })
+  })
+
+  const getTransport = (socketId) => {
+    const [producerTransport] = transports.filter(transport => transport.socketId === socketId && !transport.consumer)
+    return producerTransport.transport
+  }
+
+  // see client's socket.emit('transport-produce', ...)
+  socket.on('transport-produce', async ({ kind, rtpParameters, appData }, callback) => {
+    // call produce based on the prameters from the client
+    const producer = await getTransport(socket.id).produce({
+      kind,
+      rtpParameters,
+    })
+
+    // add producer to the producers array
+    const { roomName } = peers[socket.id]
+
+    addProducer(producer, roomName)
+
+    informConsumers(roomName, socket.id, producer.id)
+
+    console.log('Producer ID: ', producer.id, producer.kind)
+
+    producer.on('transportclose', () => {
+      console.log('transport for this producer closed ')
+      producer.close()
+    })
+
+    // Send back to the client the Producer's id
+    callback({
+      id: producer.id,
+      producersExist: producers.length>1 ? true : false
+    })
+  })
+
+  const addProducer = (producer, roomName) => {
+    producers = [
+      ...producers,
+      { socketId: socket.id, producer, roomName, }
+    ]
+
+    peers[socket.id] = {
+      ...peers[socket.id],
+      producers: [
+        ...peers[socket.id].producers,
+        producer.id,
+      ]
+    }
+  }
+
+  const informConsumers = (roomName, socketId, id) => {
+    console.log(`just joined, id ${id} ${roomName}, ${socketId}`)
+    // A new producer just joined
+    // let all consumers to consume this producer
+    producers.forEach(producerData => {
+      if (producerData.socketId !== socketId && producerData.roomName === roomName) {
+        const producerSocket = peers[producerData.socketId].socket
+        // use socket to send producer id to producer
+        producerSocket.emit('new-producer', { producerId: id })
+      }
+    })
+  }
+  
+
+
+
+
+
+
+
+
+
+
+
 });
 
 //when caller make the room
@@ -430,6 +651,7 @@ function makeGroup(socket) {
   socket.emit("accept_join", [1]);
   return groupName;
 }
+
 //when callee join the room
 function joinGroup(groupName, socket, nickname) {
   console.log("joinGroup");
@@ -454,6 +676,45 @@ function joinGroup(groupName, socket, nickname) {
       socket.emit("accept_join", groupObjArr[i].users);
     }
   }
+}
+
+// WebRTC SFU (mediasoup)
+const createWebRtcTransport = async (router) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      // https://mediasoup.org/documentation/v3/mediasoup/api/#WebRtcTransportOptions
+      const webRtcTransport_options = {
+        listenIps: [
+          {
+            ip: '0.0.0.0', // replace with relevant IP address
+            announcedIp: '10.0.0.115',
+          }
+        ],
+        enableUdp: true,
+        enableTcp: true,
+        preferUdp: true,
+      }
+
+      // https://mediasoup.org/documentation/v3/mediasoup/api/#router-createWebRtcTransport
+      let transport = await router.createWebRtcTransport(webRtcTransport_options)
+      console.log(`transport id: ${transport.id}`)
+
+      transport.on('dtlsstatechange', dtlsState => {
+        if (dtlsState === 'closed') {
+          transport.close()
+        }
+      })
+
+      transport.on('close', () => {
+        console.log('transport closed')
+      })
+
+      resolve(transport)
+
+    } catch (error) {
+      reject(error)
+    }
+  })
 }
 
 module.exports = app;
